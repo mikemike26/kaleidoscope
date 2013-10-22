2b411142ee9549a064f07a536cefaede0ca089cc
Mike Rensel <rensel.michael@gmail.com>
2013-10-22 12:29:14 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaleidoscope::Application.config.secret_token = '5303a1f0813d2f7650eff38f9e2e4395c75396f32f6be874360d51d7c7e1a08144483f6a43cf4a82f57a1fb908d29f0c2616423790ead826ea06c2a555dce49e'
